ff1487b74f55c16ba98fb22541bcaafd3d93d507
Amin Yusupov <aminyusupov2000@gmail.com>
2025-09-21 23:12:41 +0300

    initt

---
+++ test.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot=telebot.TeleBot('8374810935:AAGduBipbU_PgHGT3SBVziICNO4_5kzaSAg')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id,f'привет, {message.from_user.first_name}')
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id,'')
+
+
+
+
+bot.polling(non_stop=True)
