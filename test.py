675a3ca6dac2d636ee358b8b00060ef45ba98ad6
Amin Yusupov <aminyusupov2000@gmail.com>
2025-09-22 21:14:19 +0300

    g

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import telebot
-
+instructions ='Ты помощник по математике. Решай пошагово, пиши кратко и понятно. В конце дай ответ. '
 bot=telebot.TeleBot('8374810935:AAGduBipbU_PgHGT3SBVziICNO4_5kzaSAg')
 
+    
 
 @bot.message_handler(commands=['start'])
 def main(message):
@@ -11,7 +12,17 @@ def main(message):
 def main(message):
     bot.send_message(message.chat.id,'')
 
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    bot.send_message(message.chat.id,'вы отправили фото')
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+    if len(message.text) > 3000:
+        bot.send_message(message.chat.id, "Вы достигли лимита: максимум 3000 символов")
+        return    
+    bot.send_message (message.chat.id,'вы отправили текст')
+        
+    
 
-#наблюдай сохранить файл забыл
 
 bot.polling(non_stop=True)
