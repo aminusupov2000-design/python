fbfb429220423be7f27047f596b30dd3b67cef79
Amin Yusupov <aminyusupov2000@gmail.com>
2025-10-01 15:22:07 +0300

    добавил нейросеть

--- test.py
+++ test.py
@@ -2,10 +2,22 @@ save =""
 instructions ='Ты помощник по математике. Решай пошагово, пиши кратко и понятно. В конце дай ответ. '
 import telebot
 import logging
-from google import genai #возможно ошибка здесь 
 
-client = genai.Client(api_key="AIzaSyDnagkgb9BZu6CFMttjZAA06qOIleiZNj0")
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyDnagkgb9BZu6CFMttjZAA06qOIleiZNj0")
+model = genai.GenerativeModel("gemini-1.5")
+bot=telebot.TeleBot('8374810935:AAGduBipbU_PgHGT3SBVziICNO4_5kzaSAg')
+
+
+@bot.message_handler(content_types=['text'])
+def save_text(message):
+    if len(message.text) > 1000:
+        bot.send_message(message.chat.id, "Вы достигли лимита: максимум 1000 символов")
+        return    
+    save = message.text
+    response = model.generate_content(save)
+    bot.send_message(message.chat.id,response.text)
 
 # Настройка логирования
 logging.basicConfig(
@@ -17,14 +29,9 @@ logging.basicConfig(
 
 
 
-bot=telebot.TeleBot('8374810935:AAGduBipbU_PgHGT3SBVziICNO4_5kzaSAg')
 
-@bot.message_handler(content_types=['text'])
-def save_text(message):
-    global save
-    save = message.text
-    print(save)
-    bot.send_message(message.chat.id, f"Вы написали: {save}")
+
+
 
 
 @bot.message_handler(commands=['start'])
@@ -40,12 +47,8 @@ def main(message):
 @bot.message_handler(content_types=['photo'])
 def get_photo(message):
     bot.send_message(message.chat.id,'вы отправили фото')
-@bot.message_handler(content_types=['text'])
-def get_text(message):
-    if len(message.text) > 1000:
-        bot.send_message(message.chat.id, "Вы достигли лимита: максимум 1000 символов")
-        return    
-    bot.send_message (message.chat.id,'вы отправили текст')
-        
+#@bot.message_handler(content_types=['text'])
 
+        
 bot.polling(non_stop=True)
+
